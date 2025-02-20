07acb95d0467a631ca22a4c9e9ee3f90fbbae9cf
luminous <masha4russia@gmail.com>
2025-02-21 02:06:32 +0300

    SECOND commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/main.html')
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/404.html')
+})
+
+app.listen(5000)
+console.log('хеппи коргиг')
